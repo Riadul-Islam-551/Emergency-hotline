f3df783edd297a4c5cb8df4143172641888fccf1
Riadul Islam <riadul551@gmail.com>
2025-08-29 02:43:39 +0600

    calling button coin count functionality is complete

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// heart button functionality implementation
 const heartButton = document.getElementsByClassName("card-heart");
 
 for (const heart of heartButton) {
@@ -6,5 +7,26 @@ for (const heart of heartButton) {
     let heartCount = parseInt(heartCountElement.innerText);
     heartCount++;
     heartCountElement.innerText = heartCount;
+  });
+}
+// call button functionality
+const cardCallButton = document.getElementsByClassName("card-call");
+let finishCoins = false;
+for (const callButton of cardCallButton) {
+  callButton.addEventListener("click", function () {
+    if (finishCoins) {
+      alert("You don't have enough coin");
+      return;
+    }
+    alert("calling");
+    const coinCountcontainer = document.getElementById("coin-count");
+    let coinCount = parseInt(coinCountcontainer.innerText);
+    coinCount -= 20;
+    coinCountcontainer.innerText = coinCount;
+    if (coinCount <= 0) {
+      finishCoins = true;
+    }
+    console.log(coinCount);
+    console.log("button is clicked");
   });
 }
